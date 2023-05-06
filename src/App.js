b417d36887a08532516f194805ac66e345b9b1e9
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-05-06 22:31:30 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
-
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
+
 <Navbar title="Fire" aboutText="About Us"/>
+<div className="container">
+<TextForm heading ="Enter to analyze"/>
+</div>
+
     </>
   );
 }
+
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TextForm() {
+    return (
+        <div>
+            <div class="mb-3">
+                <label for="mytext" class="form-label">Example text area</label>
+                <textarea class="form-control" id="mybox" rows="3"></textarea>
+            </div>
+        </div>
+    )
+}
