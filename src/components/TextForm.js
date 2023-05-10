6af67eb63a71d3be4670d1ada7d1ac487879ed89
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-05-10 21:57:12 +0545

    hooks

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function TextForm() {
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("uppercase clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+
+    }
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('Enter text here');
     return (
         <div>
-            <div class="mb-3">
-                <label for="mytext" class="form-label">Example text area</label>
-                <textarea class="form-control" id="mybox" rows="3"></textarea>
+            <h1>{props.heading} </h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="6"></textarea>
             </div>
+            <button className='btn btn-primary' onClick={handleUpClick}> Convert to UpperCase</button>
         </div>
     )
 }
