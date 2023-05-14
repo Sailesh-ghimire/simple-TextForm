09de0018d8c619608ff2bb5cc39a015cd352e3aa
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-05-14 14:04:47 +0545

    dark

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+    }
+    else {
+      setMode('dark');
+    }
+  }
   return (
     <>
+      {/* <Navbar title="Fire" aboutText="About Us" /> */}
+      {/* <Navbar/> */}
 
-      <Navbar title="Fire" aboutText="About Us" />
-      <div className="container">
-      <TextForm heading="Enter to analyze" />
+      <Navbar title="Fire" mode={mode} toggleMode={toggleMode} />
 
-      {/* <About/> */}
+      <div className="container my-3">
+        <TextForm heading="Enter to analyze" />
+
+        {/* <About/> */}
       </div>
 
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,18 +19,27 @@ export default function TextForm(props) {
 
     }
 
+    const copyText = ()=>{
+        let text=document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
     const [text, setText] = useState('');
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode === 'dark' ? 'white' : 'black'}} >
             <h1>{props.heading} </h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="6"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white'
+            , color: props.mode === 'dark' ? 'white' : 'black'}} id="mybox" rows="6"></textarea>
             </div>
             <button className='btn btn-primary mx-2' onClick={handleUpClick}> Convert to UpperCase</button>
             <button className='btn btn-primary mx-2' onClick={handleLowClick}> Convert to LowerCase</button>
+            <button className='btn btn-primary mx-2' onClick={copyText}> Copy Text</button>
+
         </div>
-        <div className="container">
+        <div className="container" style={{color: props.mode === 'dark' ? 'white' : 'black'}} >
             <h1>Your total words</h1>
             <p>{text.length} characters</p>
         </div>
