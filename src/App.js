7c3786c01dd51c0d4cd2bd144ab3a8267b97e1e4
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2023-05-18 21:27:42 +0545

    rouer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import Alert from './components/Alert';
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1000);
+    }, 1000);
   }
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#495057';
-      showAlert("Dark mode enabled", "success");
+      // showAlert("Dark mode enabled", "success");
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Light mode enabled", "success");
+      // showAlert("Light mode enabled", "success");
 
     }
   }
@@ -37,13 +44,21 @@ function App() {
       {/* <Navbar title="Fire" aboutText="About Us" /> */}
       {/* <Navbar/> */}
 
-      <Navbar title="Fire" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter to analyze" mode={mode} />
+      <Router>
+        <Navbar title="Fire" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />}>
 
-        {/* <About/> */}
+            </Route>
+            <Route path="/" element={<TextForm showAlert={showAlert} heading=" Enter to analyze" mode={mode} />}>
+          </Route>
+        </Routes>
       </div>
+    </Router >
+      {/* <About/> */ }
+
 
     </>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -71,7 +71,7 @@ export default function About() {
                 </div>
             </div>
             <div className='container my-3'>
-            <button onClick={toggleStyle} type="button" className="btn btn-primary">{btntext}</button>
+            <button onClick={toggleStyle} type="button" className="btn btn-primary my-3">{btntext}</button>
             </div>
         </div>
     )
